3bf4d6f938d3ebae6b7bc2a1e9f0f4baf02a65ec
Andika Dwiki <56144412+andikadwikid@users.noreply.github.com>
2023-03-03 20:33:11 +0700

    recode-getAbsen

--- controllers/absensi.js
+++ controllers/absensi.js
@@ -43,16 +43,6 @@ exports.postAbsen = async (req, res) => {
 };
 
 exports.getAbsen = async (req, res) => {
-  let today = new Date();
-  let dd = String(today.getDate()).padStart(2, "0");
-  let mm = String(today.getMonth() + 1).padStart(2, "0");
-  let yyyy = today.getFullYear();
-
-  let hours = String(today.getHours());
-  let minutes = String(today.getMinutes());
-  let seconds = String(today.getSeconds());
-
-  today = `${yyyy}-${mm}-${dd} ${hours}:${minutes}:${seconds}`;
 
   const absensi = await Absensi.findAll();
 
